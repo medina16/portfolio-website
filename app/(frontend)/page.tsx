2e3807b969bf51833e1350989576f6a5ae795c5d
Medina FM <medina.fm.167@gmail.com>
2026-08-28 09:52:54 +0700

    fix static home + project jadi dynamic

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLandmarkFlag,
--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
